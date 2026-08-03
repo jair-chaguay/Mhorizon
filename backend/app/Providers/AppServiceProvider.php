01ca279c09dd404efae6d657909a643e32638859
jair-chaguay <jchaguay@espol.edu.ec>
2026-08-03 15:42:46 -0500

    udp

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,6 @@ class AppServiceProvider extends ServiceProvider
     {
         if (str_contains(config('app.url'), 'https://')) {
         URL::forceScheme('https');
-    }
+        }
     }
 }
