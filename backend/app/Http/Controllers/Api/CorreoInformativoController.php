3a76a6700843a4612f96af43602452172c2bb383
Jair Chaguay <jchaguay@espol.edu.ec>
2026-09-07 16:05:07 -0500

    upd

--- backend/app/Http/Controllers/Api/CorreoInformativoController.php
+++ backend/app/Http/Controllers/Api/CorreoInformativoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\CorreoInformativo;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
