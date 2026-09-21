23b50fdd27f4886462339b8eb9eb5e7ea48adabb
Jair Chaguay <jchaguay@espol.edu.ec>
2026-09-21 16:09:47 -0500

    update

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -25,6 +25,7 @@ use App\Http\Controllers\Api\TrackingController;
 
 Route::get('/track/open', [TrackingController::class, 'trackOpen']);
 Route::get('/track/click', [TrackingController::class, 'trackClick']);
+Route::get('/leads', [TrackingController::class, 'getDashboardData']);
 //LOGIN
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/sitemap', [SitemapController::class, 'index']);
